abda9c7b519469a7d0f18da4a9f784e59e2c5d7f
harvey he <zidongchn@gmail.com>
2018-11-13 21:07:14 +0800

    123

---
+++ src/js/constants/Config.js
@@ -0,0 +1,10 @@
+// 配置
+export const CONFIG = {
+  owner      : 'cobish',
+  repo       : 'cobish.github.io',
+  title      : 'cobish - 写给未来的自己',
+  titleLoad  : 'cobish - 正在加载...',
+  baiduAnaly : '84602bc3713ca2aeb0258c55adf6a333',
+  token: ['9367908bbd1f87aae9c4', '561b1e0e27252e30b6ce'] // 需拆开
+};
+
